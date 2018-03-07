04f64a98215691fbe281b14dc2a13c2d4243e9b3
YannickBezes <yannick.bezes@gmail.com>
2018-03-07 09:32:10 +0100

    update libelle

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => "Le mot de passe ou l'email est invalide.",
+    'throttle' => "Trop de tentative de connexion. Veuillez attendre :seconds seconds.",
+
+];
