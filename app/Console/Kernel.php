dcfb02fa6295624776a71a76cee24c93dcea40ba
YannickBezes <yannick.bezes@gmail.com>
2018-03-12 11:46:53 +0100

    update kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,8 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
+class Kernel extends ConsoleKernel {
     /**
      * The Artisan commands provided by your application.
      *
@@ -22,10 +21,8 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('NetatmoData:setData')
-                  ->cron('0 */2 * * *');
+    protected function schedule(Schedule $schedule) {
+        $schedule->command('NetatmoData:setData')->cron('0 */2 * * *');
     }
 
     /**
